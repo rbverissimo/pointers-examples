b3327958fe239b2dfcd15a98234a514b4b928cf7
rbverissimo <rverissimo.dev@gmail.com>
2023-04-27 21:41:05 -0300

    Understanding a bit more of how a stack frame is manipulated

--- pointers-as-function-arguments.cpp
+++ pointers-as-function-arguments.cpp
@@ -1,17 +1,26 @@
 #include<stdio.h>
 
-void incrementByOne(int a){
+void incrementByOne(int* a){
 	
-	a++;
-	printf("Address of variable a inside main: %d\n", &a);
+	*a = (*a) + 1;
+	printf("Address of variable a inside main: %d\n", a);
+	
+	if((*a) < 30 ){
+		printf("Value of a: %d\n", *a);
+		incrementByOne(a);
+		
+	}
 	
 }
 
 int main(){
 	
 	int a = 10;
-	incrementByOne(a);
+	int* p = &a; 
+	incrementByOne(p);
 	printf("Address of variable a inside main: %d\n", &a);
+	
+	printf("Value of a, after increment: %d\n", a);
 	
 	return 0;
 }
