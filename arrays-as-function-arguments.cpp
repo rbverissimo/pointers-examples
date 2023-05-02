5a4222041e2c27760d0721ae79b0808bf71de2fe
rbverissimo <rverissimo.dev@gmail.com>
2023-05-02 06:04:24 -0300

    Reverting changes. Now the code runs.

--- arrays-as-function-arguments.cpp
+++ arrays-as-function-arguments.cpp
@@ -1,10 +1,8 @@
 #include<stdio.h>
 
-int sumOfElements(int array[]){
-				// (int* array)
+int sumOfElements(int*  array, int size){
 				
 	int result = 0; 
-	int size = sizeof(array)/sizeof(array[0]);
 	for(int i = 0; i < size; i++){
 		result = result + (*(array + i));
 	}
@@ -16,8 +14,9 @@ int sumOfElements(int array[]){
 
 int main(){
 	
-	int a[] = {2, 3, 5, 5};
-	printf("The sum of elements in array is: %d\n", sumOfElements(a));
+	int array[] = {2, 3, 5, 5};
+	int size = sizeof(array)/sizeof(array[0]);
+	printf("The sum of elements in array is: %d\n", sumOfElements(array, size));
 	
 	return 0;
 }
