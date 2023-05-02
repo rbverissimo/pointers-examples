d139720b42177d2c29ac2a8d3427e9cf03d2c3d6
rbverissimo <rverissimo.dev@gmail.com>
2023-05-02 06:14:59 -0300

    Understanding a bit more of arrays, pointers and passing by reference

--- arrays-as-function-arguments.cpp
+++ arrays-as-function-arguments.cpp
@@ -12,11 +12,29 @@ int sumOfElements(int*  array, int size){
 	
 }
 
+void Double(int* array, int size){
+	for(int i = 0; i < size; i++){
+		array[i] = 2 * (*(array + i));
+	}
+}
+
+void printElementsArray(int* array, int size){
+	for(int i = 0; i < size; i++){
+		printf("Element at index %d value: %d\n", i, *(array + i));
+	}
+}
+
 int main(){
 	
 	int array[] = {2, 3, 5, 5};
 	int size = sizeof(array)/sizeof(array[0]);
-	printf("The sum of elements in array is: %d\n", sumOfElements(array, size));
+	printf("The sum of elements in array is: %d\n", sumOfElements(&array[0], size));
+	// &array[0] means I'm passing the first address of the array to a function
+	// I can double the value of all elements in the array by passing it by reference: 
+	Double(&array[0], size);
+	printElementsArray(&array[0], size);
+	
+	
 	
 	return 0;
 }
